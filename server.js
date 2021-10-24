f4f0a21312cbfae53d75b1c26e1acf8348ec0aa0
authoritydmc <com.mailuser@gmail.com>
2021-10-24 22:42:46 +0530

    serving

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/out-tsc'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/out-tsc/' }
+    );
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
